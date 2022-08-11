234926adfab07ce63b2650f7371e126e6f0784a9
yangwei <yangwei1@yssdata.com>
2022-08-11 16:01:00 +0800

    新增predicate-demo模块

--- function-demo/src/main/java/com/example/TestFunction.java
+++ function-demo/src/main/java/com/example/TestFunction.java
@@ -12,8 +12,13 @@ public class TestFunction {
         return func1.compose(func2).apply(i);
     }
 
+    public static int testFunctionAndThen(int i, Function<Integer, Integer> func1, Function<Integer, Integer> func2) {
+        return func1.andThen(func2).apply(i);
+    }
+
     public static void main(String[] args) {
         System.out.println("testFunctionApply = " + testFunctionApply(2, i -> i * 2 + 1));
         System.out.println("testFunctionCompose = " + testFunctionCompose(2, i -> i * 2 + 1, i -> i * 2));
+        System.out.println("testFunctionAndThen = " + testFunctionAndThen(2, i -> i * 2 + 1, j -> j * 2));
     }
 }
