84cd7552f8aacef4b6765f1740174acb64d46a36
yangwei <yangwei1@yssdata.com>
2022-08-11 15:27:39 +0800

    新增function-demo模块

---
+++ function-demo/src/main/java/com/example/TestFunction.java
@@ -0,0 +1,19 @@
+package com.example;
+
+import java.util.function.Function;
+
+public class TestFunction {
+
+    public static int testFunctionApply(int i, Function<Integer, Integer> func) {
+        return func.apply(i);
+    }
+
+    public static int testFunctionCompose(int i, Function<Integer, Integer> func1, Function<Integer, Integer> func2) {
+        return func1.compose(func2).apply(i);
+    }
+
+    public static void main(String[] args) {
+        System.out.println("testFunctionApply = " + testFunctionApply(2, i -> i * 2 + 1));
+        System.out.println("testFunctionCompose = " + testFunctionCompose(2, i -> i * 2 + 1, i -> i * 2));
+    }
+}
