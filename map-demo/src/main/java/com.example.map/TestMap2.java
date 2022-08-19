1de94e7b0a08cf838604ac1448d54f686126b3e3
yangwei <yangwei1@yssdata.com>
2022-08-20 00:24:56 +0800

    新建一个测试用的map

---
+++ map-demo/src/main/java/com.example.map/TestMap2.java
@@ -0,0 +1,13 @@
+package com.example.map;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class TestMap2 {
+    public static void main(String[] args) {
+        Map<String, String> map = new HashMap<>();
+        map.put("apple", "100");
+        map.put("orange", "50");
+        map.put("banana", "200");
+    }
+}
