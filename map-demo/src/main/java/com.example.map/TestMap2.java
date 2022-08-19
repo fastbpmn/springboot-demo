184dfeb66e97b1690c70b40454c0c6add30c27ea
yangwei <yangwei1@yssdata.com>
2022-08-20 00:37:37 +0800

    08202使用Map.Entry循环map

--- map-demo/src/main/java/com.example.map/TestMap2.java
+++ map-demo/src/main/java/com.example.map/TestMap2.java
@@ -9,5 +9,10 @@ public class TestMap2 {
         map.put("apple", "100");
         map.put("orange", "50");
         map.put("banana", "200");
+
+        // 下面为08202提交
+        for (Map.Entry<String, String> entry : map.entrySet()) {
+            System.out.println("key = " + entry.getKey() + ", value = " + entry.getValue());
+        }
     }
 }
