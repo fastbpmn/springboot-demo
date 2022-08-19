3f8f213fe716a426a8439bcda748f82be83eea83
yangwei <yangwei1@yssdata.com>
2022-08-20 00:45:18 +0800

    更新map

--- map-demo/src/main/java/com.example.map/TestMap2.java
+++ map-demo/src/main/java/com.example.map/TestMap2.java
@@ -9,6 +9,7 @@ public class TestMap2 {
         map.put("apple", "100");
         map.put("orange", "50");
         map.put("banana", "200");
+        map.put("grape", "300");
 
         // 下面为08202提交
         for (Map.Entry<String, String> entry : map.entrySet()) {
