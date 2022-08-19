3bc9593e5c279e3ada239fc5e3af889b0193b076
yangwei <yangwei1@yssdata.com>
2022-08-19 23:25:30 +0800

    通过继承Thread启动线程

--- thread-demo/src/main/java/com/example/thread/ThreadDemo.java
+++ thread-demo/src/main/java/com/example/thread/ThreadDemo.java
@@ -5,7 +5,6 @@ import com.example.thread.pojo.Task;
 public class ThreadDemo {
     public static void main(String[] args) {
         Thread thread = new Task();
-        // 这是2022081901的开发分支
         thread.start(); // 这是2022081902的开发分支
     }
 }
